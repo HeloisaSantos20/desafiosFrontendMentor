d0185cf65d3847865452083ec1c5140642c325b1
HeloisaSantos20 <128840247+HeloisaSantos20@users.noreply.github.com>
2023-04-05 08:43:32 -0300

    commit

--- InteractiveRatingComponent/index.js
+++ InteractiveRatingComponent/index.js
@@ -1,8 +1,17 @@
 const mainContainer = document.querySelector(".mainContainer");
 const thankContainer = document.querySelector(".thankContainer");
 const submitButton = document.getElementById("submit");
+const rating = document.getElementById("rating");
+const rates = document.querySelectorAll(".btn");
 
+/*screen change*/
 submitButton.addEventListener("click",()=>{
     thankContainer.classList.remove("hidden");
     mainContainer.style.display = "none";
+})
+/* You selected X out 5 */
+rates.forEach((rate) =>{
+    rate.addEventListener("click", () => {
+        rating.textContent = rate.textContent;
+    })
 })
